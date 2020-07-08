47ad6dbe8fa342e21fd9aa77db2a74e3634a6291
Ana Julia Terra <ajtalves@hotmail.com>
2020-07-07 22:30:54 -0300

    correção botao voltar

--- classe-Adm-inserir.php
+++ classe-Adm-inserir.php
@@ -141,7 +141,7 @@ https://templatemo.com/tm-516-known
 
             
                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                        <a href="classe-adm-curso.php" class="btn btn-secondary" >Voltar</a>
                                         <button type="submit" class="btn btn-info">Gravar</button>
                                    </div>
                               </form>
